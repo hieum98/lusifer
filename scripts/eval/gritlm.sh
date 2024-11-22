18a92b16119b72c58ee646f77e677904aeba5bf4
hieuman <hieuman2708@gmail.com>
2024-11-22 11:33:52 -0800

    fix: reduce batch size from 512 to 256 in GritLM evaluation script

--- scripts/eval/gritlm.sh
+++ scripts/eval/gritlm.sh
@@ -1,6 +1,6 @@
 python -m src.eval.eval \
     --model_name_or_path GritLM/GritLM-7B \
     --output_folder /sensei-fs/users/chienn/hieu/lusifer/gritlm \
-    --batch_size 512 \
+    --batch_size 256 \
     --max_length 512 \
     --langs all
