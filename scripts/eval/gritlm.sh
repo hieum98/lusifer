9e76203dd949a8e96b037f45252a89ec8273b67b
hieuman <hieuman2708@gmail.com>
2024-11-22 11:04:16 -0800

    add: create script for evaluating GritLM model

---
+++ scripts/eval/gritlm.sh
@@ -0,0 +1,6 @@
+python -m src.eval.eval \
+    --model_name_or_path GritLM/GritLM-7B \
+    --output_folder /sensei-fs/users/chienn/hieu/lusifer/gritlm \
+    --batch_size 512 \
+    --max_length 512 \
+    --langs all
